4182bc41143c595e9e66135d361b6950cc89326d
baigali <baigalikkk@163.com>
2018-08-27 11:57:34 +0800

    Add current filename and line number

--- src/helper.php
+++ src/helper.php
@@ -6,6 +6,10 @@ use Symfony\Component\VarDumper\Cloner\VarCloner;
 if (!function_exists('dd')) {
     function dd(...$args)
     {
+        $trace = debug_backtrace();
+
+        $args = array_merge([$trace[0]['file'].':'.$trace[0]['line']], $args);
+        
         foreach ($args as $x) {
             (new Dumper)->dump((new VarCloner)->cloneVar($x));
         }

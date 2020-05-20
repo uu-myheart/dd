59b689272db344bed54c8d78bd23a031ca8c7a2d
GaranPoe <baigalikkk@163.com>
2020-05-20 10:27:04 +0800

    Add cli dumper

--- src/helper.php
+++ src/helper.php
@@ -2,16 +2,25 @@
 
 use Garan\Dumper;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
 
 if (!function_exists('dd')) {
     function dd(...$args)
     {
-        $trace = debug_backtrace();
+        if (\in_array(\PHP_SAPI, ['cli', 'phpdbg'], true)) {
+            $dumper = new CliDumper();
+        } else {
+            $trace = debug_backtrace();
 
-        $args = array_merge([$trace[0]['file'].':'.$trace[0]['line']], $args);
-        
+            $args = array_merge([$trace[0]['file'].':'.$trace[0]['line']], $args);
+
+            $dumper = new Dumper();
+        }
+
+        $cloner = new VarCloner();
+
         foreach ($args as $x) {
-            (new Dumper)->dump((new VarCloner)->cloneVar($x));
+            $dumper->dump($cloner->cloneVar($x));
         }
 
         die(1);

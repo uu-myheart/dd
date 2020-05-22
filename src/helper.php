8f9cb146eb1fff2c3efbab3b5b19b026fab78dd6
GaranPoe <baigalikkk@163.com>
2020-05-22 16:26:16 +0800

    Add debug_backtrace in cli mode

--- src/helper.php
+++ src/helper.php
@@ -7,15 +7,12 @@ use Symfony\Component\VarDumper\Dumper\CliDumper;
 if (!function_exists('dd')) {
     function dd(...$args)
     {
-        if (\in_array(\PHP_SAPI, ['cli', 'phpdbg'], true)) {
-            $dumper = new CliDumper();
-        } else {
-            $trace = debug_backtrace();
+        $dumper = \in_array(\PHP_SAPI, ['cli', 'phpdbg'], true)
+            ? new CliDumper()
+            : new Dumper();
 
-            $args = array_merge([$trace[0]['file'].':'.$trace[0]['line']], $args);
-
-            $dumper = new Dumper();
-        }
+        $trace = debug_backtrace();
+        $args = array_merge([$trace[0]['file'].':'.$trace[0]['line']], $args);
 
         $cloner = new VarCloner();
 
